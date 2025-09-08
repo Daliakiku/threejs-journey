9961f7ed14b2631d39120fc38d887888d73930bc
dalia <dalia.thornburgh@gmail.com>
2025-09-08 17:13:36 -0600

    c0mpleted first class

---
+++ basics-1/first-project/script.js
@@ -0,0 +1,42 @@
+console.log("pop that javascriptussy queen");
+
+import * as THREE from 'three';
+
+//canvas
+const canvas = document.querySelector('canvas.canvas');
+console.log(canvas);
+
+
+//create scene
+const scene = new THREE.Scene();
+
+//object/mesh = geometry + material
+const geometry = new THREE.BoxGeometry(1, 1, 1); //width, height, depth
+const material = new THREE.MeshBasicMaterial({ color: "red"});
+const mesh = new THREE.Mesh(geometry, material);
+
+//add mesh to scene
+scene.add(mesh);
+
+//rotate mesh
+mesh.rotation.y = 0.5;
+
+//camera
+const sizes = { //creating an object to store sizes
+    width: 800,
+    height: 600
+}
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height); //fov, aspect ratio
+//add camera to the scene
+scene.add(camera);
+camera.position.z = 3; //move the camera away from the cube
+
+//renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+});
+renderer.setSize(sizes.width, sizes.height);
+
+//create render
+renderer.render(scene, camera);
